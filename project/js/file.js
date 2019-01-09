2dce901ab5eed5b4e9be2f7c9ea3fccfa1eef396
YaJun <502113690@qq.com>
2019-01-09 15:17:36 +0800

    add select

---
+++ project/js/file.js
@@ -0,0 +1,339 @@
+	//商品分类选项卡
+	var aAtn = [
+		[
+			{
+				url:'#',
+				img:'images/mix3-320.png',
+				name:'小米MX 2',
+				price:'3299',
+				tag:'新品'
+			},
+			{
+				url:'#',
+				img:'images/mix3-320.png',
+				name:'小米MX 2',
+				price:'3299',
+			},
+			{
+				url:'#',
+				img:'images/mix3-320.png',
+				name:'小米MX 3',
+				price:'3299',
+				tag:'新品'
+			},
+			{
+				url:'#',
+				img:'images/mix3-320.png',
+				name:'小米MX 3',
+				price:'3299',
+			},
+			{
+				url:'#',
+				img:'images/mix3-320.png',
+				name:'小米MX 3',
+				price:'3299',
+				tag:'新品'
+			},
+			{
+				url:'#',
+				img:'images/mix3-320.png',
+				name:'小米MX 3',
+				price:'3299',
+				tag:'新品'
+			},
+		],//小米手机
+		[
+			{
+				url:'#',
+				img:'images/p2.jpg',
+				name:'小米MX 2',
+				price:'3299',
+				tag:'新品'
+			},
+			{
+				url:'#',
+				img:'images/p2.jpg',
+				name:'小米MX 3',
+				price:'3299',
+				tag:'新品'
+			},
+			{
+				url:'#',
+				img:'images/p2.jpg',
+				name:'小米MX 2',
+				price:'3299',
+			},
+			{
+				url:'#',
+				img:'images/p2.jpg',
+				name:'小米MX 3',
+				price:'3299',
+				tag:'新品'
+			},
+			{
+				url:'#',
+				img:'images/p2.jpg',
+				name:'小米MX 3',
+				price:'3299',
+			},
+			{
+				url:'#',
+				img:'images/p2.jpg',
+				name:'小米MX 3',
+				price:'3299',
+				tag:'新品'
+			},
+		],//红米
+		[
+			{
+				url:'#',
+				img:'images/f2.jpg',
+				name:'小米MX 2',
+				price:'3299',
+				tag:'新品'
+			},
+			{
+				url:'#',
+				img:'images/f2.jpg',
+				name:'小米MX 2',
+				price:'3299',
+			},
+			{
+				url:'#',
+				img:'images/f2.jpg',
+				name:'小米MX 3',
+				price:'3299',
+				tag:'新品'
+			},
+			{
+				url:'#',
+				img:'images/f2.jpg',
+				name:'小米MX 3',
+				price:'3299',
+			},
+			{
+				url:'#',
+				img:'images/f2.jpg',
+				name:'小米MX 3',
+				price:'3299',
+				tag:'新品'
+			},
+			{
+				url:'#',
+				img:'images/f2.jpg',
+				name:'小米MX 3',
+				price:'3299',
+				tag:'新品'
+			},
+		],//电视
+		[
+			{
+				url:'#',
+				img:'images/f3.png',
+				name:'小米MX 2',
+				price:'3299',
+				tag:'新品'
+			},
+			{
+				url:'#',
+				img:'images/f3.png',
+				name:'小米MX 3',
+				price:'3299',
+				tag:'新品'
+			},
+			{
+				url:'#',
+				img:'images/f4.jpg',
+				name:'小米MX 2',
+				price:'3299',
+			},
+			{
+				url:'#',
+				img:'images/f3.png',
+				name:'小米MX 3',
+				price:'3299',
+				tag:'新品'
+			},
+			{
+				url:'#',
+				img:'images/f4.jpg',
+				name:'小米MX 3',
+				price:'3299',
+			},
+			{
+				url:'#',
+				img:'images/f3.png',
+				name:'小米MX 3',
+				price:'3299',
+				tag:'新品'
+			},
+		],//笔记本
+		[
+			{
+				url:'#',
+				img:'images/f5.png',
+				name:'小米MX 2',
+				price:'3299',
+				tag:'新品'
+			},
+			{
+				url:'#',
+				img:'images/f5.png',
+				name:'小米MX 2',
+				price:'3299',
+			},
+			{
+				url:'#',
+				img:'images/f5.png',
+				name:'小米MX 3',
+				price:'3299',
+				tag:'新品'
+			},
+			{
+				url:'#',
+				img:'images/f5.png',
+				name:'小米MX 3',
+				price:'3299',
+			},
+			{
+				url:'#',
+				img:'images/f5.png',
+				name:'小米MX 3',
+				price:'3299',
+				tag:'新品'
+			},
+			{
+				url:'#',
+				img:'images/f5.png',
+				name:'小米MX 3',
+				price:'3299',
+				tag:'新品'
+			},
+		],//空调
+		[
+			{
+				url:'#',
+				img:'images/f6.jpg',
+				name:'小米MX 2',
+				price:'3299',
+				tag:'新品'
+			},
+			{
+				url:'#',
+				img:'images/f6.jpg',
+				name:'小米MX 3',
+				price:'3299',
+				tag:'新品'
+			},
+			{
+				url:'#',
+				img:'images/f6.jpg',
+				name:'小米MX 2',
+				price:'3299',
+			},
+			{
+				url:'#',
+				img:'images/f6.jpg',
+				name:'小米MX 3',
+				price:'3299',
+				tag:'新品'
+			},
+			{
+				url:'#',
+				img:'images/f6.jpg',
+				name:'小米MX 3',
+				price:'3299',
+			},
+			{
+				url:'#',
+				img:'images/f6.jpg',
+				name:'小米MX 3',
+				price:'3299',
+				tag:'新品'
+			},
+		],//新品
+		[
+			{
+				url:'#',
+				img:'images/4Q.png',
+				name:'小米MX 2',
+				price:'3299',
+				tag:'新品'
+			},
+			{
+				url:'#',
+				img:'images/4Q.png',
+				name:'小米MX 2',
+				price:'3299',
+			},
+			{
+				url:'#',
+				img:'images/4Q.png',
+				name:'小米MX 3',
+				price:'3299',
+				tag:'新品'
+			},
+			{
+				url:'#',
+				img:'images/4Q.png',
+				name:'小米MX 3',
+				price:'3299',
+			},
+			{
+				url:'#',
+				img:'images/4Q.png',
+				name:'小米MX 3',
+				price:'3299',
+				tag:'新品'
+			},
+			{
+				url:'#',
+				img:'images/4Q.png',
+				name:'小米MX 3',
+				price:'3299',
+				tag:'新品'
+			},
+		],//路由器
+		[
+			{
+				url:'#',
+				img:'images/xiaomijingshuiqi.jpg',
+				name:'小米MX 2',
+				price:'3299',
+				tag:'新品'
+			},
+			{
+				url:'#',
+				img:'images/xiaomijingshuiqi.jpg',
+				name:'小米MX 3',
+				price:'3299',
+				tag:'新品'
+			},
+			{
+				url:'#',
+				img:'images/xiaomijingshuiqi.jpg',
+				name:'小米MX 2',
+				price:'3299',
+			},
+			{
+				url:'#',
+				img:'images/xiaomijingshuiqi.jpg',
+				name:'小米MX 3',
+				price:'3299',
+				tag:'新品'
+			},
+			{
+				url:'#',
+				img:'images/xiaomijingshuiqi.jpg',
+				name:'小米MX 3',
+				price:'3299',
+			},
+			{
+				url:'#',
+				img:'images/xiaomijingshuiqi.jpg',
+				name:'小米MX 3',
+				price:'3299',
+				tag:'新品'
+			},
+		]//智能硬件
+	];
