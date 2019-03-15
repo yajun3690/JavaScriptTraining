5456f00b705d666b9bc0ba2e72a5e566ff961ee5
YaJun <502113690@qq.com>
2019-03-15 18:06:30 +0800

    add es6,7,8

--- boke/js/index.js
+++ boke/js/index.js
@@ -2,4 +2,9 @@
 	$('.carousel').carousel({
 	  interval: 2000
 	})
+	$('#active-nav-ul li').on('click',function(){
+		$('#active-nav-ul li').removeClass('active');
+		console.log(this)
+		$(this).addClass('active');
+	})
 })(jQuery);
