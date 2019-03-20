76fda7ed7891a11bdee7240e12089cda937ea22a
YaJun <502113690@qq.com>
2019-03-20 21:01:25 +0800

    add new lisd

--- 012NodeJs/002npm/001npm/m02.js
+++ 012NodeJs/002npm/001npm/m02.js
@@ -50,6 +50,37 @@ eee.on('test',(arr1,arr2)=>{
 })
 // 1,利用数组扩展运算传参
 const arr = ['hellow','kuazhu']
-// 2,对应传参，自定义事件传参没有enent
+// 2,对应传参，自定义事件传参没有enent	
 // eee.emit('test','hellow','kuazhu')
 eee.emit('test',...arr)
+
+
+
+// 只处理一次，避免无限循环。
+eee.once('newListener', (event, listener) => {
+  if (event === 'event') {
+    // 在前面插入一个新的监听器。
+    eee.on('event', () => {
+      console.log('B');
+    });
+  }
+});
+eee.on('event', () => {
+  console.log('A');
+});
+eee.emit('event');
+// 打印:
+//   B
+//   A
+eee.on('newListener', (event, cb) => {
+	console.log('newListener');
+	console.log(event);
+	cb();
+});
+
+eee.on('test1',()=>{
+	console.log('test1::hellow')
+})
+eee.on('test2',()=>{
+	console.log('test2::hellow')
+})
