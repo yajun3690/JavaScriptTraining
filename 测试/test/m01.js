0f7d0eb8ba1513cda7713ed8c54acc488ab7d138
YaJun <502113690@qq.com>
2019-03-20 18:02:20 +0800

    add node sjs

---
+++ 测试/test/m01.js
@@ -0,0 +1,16 @@
+
+
+const str = 'hellow';
+
+const fn=()=>{
+	console.log('fn::',str)
+}
+const obj ={
+	name:'tom',
+	age:18
+}
+module.exports = {
+	fn,
+	str,
+	obj
+}
---
+++ 测试/test/m02.js
@@ -0,0 +1,5 @@
+
+const m01 = require('./m01.js')
+m01.fn();
+console.log('str::',m01.str)
+console.log('obj::',m01.obj)
