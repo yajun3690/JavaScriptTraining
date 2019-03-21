749f6547b0560b9e6ed64ee91cb26de7f4ea7c95
YaJun <502113690@qq.com>
2019-03-21 18:01:12 +0800

    add filename

---
+++ 012NodeJs/002npm/001npm/massage.js
@@ -0,0 +1,20 @@
+// 当前文件名绝对路径位置（包含本文件）
+// console.log(__filename)
+// 当前文件绝对路径
+// console.log(__dirname)
+console.log(module)
+// 当前文件包含信息
+
+  // id: '.',
+  // exports: {},
+  // parent: null,
+  // filename: 'D:\\text0\\012Nodejs\\002npm\\001npm\\massage.js',
+  // loaded: false,
+  // children: [],
+  // paths:
+  //  [ 'D:\\text0\\012Nodejs\\002npm\\001npm\\node_modules',
+  //    'D:\\text0\\012Nodejs\\002npm\\node_modules',
+  //    'D:\\text0\\012Nodejs\\node_modules',
+  //    'D:\\text0\\node_modules',
+  //    'D:\\node_modules' ] }
+
--- 012NodeJs/002npm/001npm/process.js
+++ 012NodeJs/002npm/001npm/process.js
@@ -3,4 +3,19 @@
 // console.log(process === global.process)
 
 // 启动文件时的传参数组 1，nodejs路径 2，文件路径 3，传参
-console.log(process.argv)
+// console.log(process.argv)
+
+// 用户环境对象  配置信息
+// console.log(process.env)
+// 进程PID
+// console.log(process.pid)
+// console.log(process.argv)
+// console.log(process.env)
+// console.log(process.pid)
+console.log(1)
+// 添加到下一个时间点的队列当中
+process.nextTick(()=>{
+	console.log(2)
+})
+console.log(3)
+
